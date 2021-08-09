e232e328a2ac98230b5bf0bdbd7b6d1209b40cb9
ashmitkx <ashmitk0507@gmail.com>
2021-08-09 15:03:13 +0530

    ProfilePage fetches from dataAPI, has drier css

--- client/src/components/ProfilePage/ProfilePage.js
+++ client/src/components/ProfilePage/ProfilePage.js
@@ -1,32 +1,50 @@
+import { useEffect, useState } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import styles from './ProfilePage.module.css';
 import classnames from 'classnames/bind';
 
+import { dataAPI } from '../../api.js';
+import CardsDisplay from '../Layouts/CardsDisplay';
+import Card from '../Layouts/Card';
 import Recents from '../Home/Recents.js';
 
 const cx = classnames.bind(styles);
 
-const Profile = ({ user }) => (
-    <article className={cx('profile')}>
-        <img src={user.imgUrl} alt='me' />
-        <div className={cx('text')}>
-            <span className={cx('name')}>{user.name}</span>
-            <span className={cx('email')}>{user.email}</span>
-        </div>
-        <button>Log Out</button>
-    </article>
-);
+const ProfilePage = () => {
+    const [user, setUser] = useState({});
 
-const ProfilePage = ({ user }) => {
-    user = {
-        name: 'Ashmit Khandelwal',
-        email: 'ashmitk0507@gmail.com',
-        imgUrl: 'https://lh3.googleusercontent.com/a-/AOh14Ggr_NSxpTqNuk3_NLO_hjGR3GN8Lh3BdnNDUVrDF70=s384-c'
-    };
+    // get user details on mount
+    useEffect(() => {
+        const getUserDetails = async () => {
+            try {
+                const res = await dataAPI.get('/user/');
+                const userData = res.data;
+                // change resolution of profile img to 384px
+                userData.imgUrl = userData.imgUrl.replace(/=s[0-9]+/, '=s384');
+                setUser(userData);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+        getUserDetails();
+    }, []);
+
+    const handleLogOut = () => window.open('http://localhost:5000/auth/logout', '_self');
 
     return (
         <main>
-            <Profile user={user} />
+            <CardsDisplay layout='single'>
+                <Card hero img={{ src: user.imgUrl, alt: 'me' }}>
+                    <div className={cx('text')}>
+                        <span className={cx('name')}>{user.name}</span>
+                        <span className={cx('email')}>{user.email}</span>
+                    </div>
+                    <button className={cx('button')} onClick={handleLogOut}>
+                        Log Out
+                    </button>
+                </Card>
+            </CardsDisplay>
+
             <Recents />
 
             {/* Catch all unknown routes and redirect to /profile */}
