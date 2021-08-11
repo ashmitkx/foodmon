1cd0b72864d18b0a4c3f6c7906bd67ac14c3ea24
ashmitkx <ashmitk0507@gmail.com>
2021-08-11 23:42:39 +0530

    dish component cleanup

--- client/src/components/Dish/Dish.js
+++ client/src/components/Dish/Dish.js
@@ -11,65 +11,12 @@ import Card from '../Layouts/Card';
 
 const cx = classnames.bind(styles);
 
-const QuantitySelect = ({ type, onUpdate, onAdd, disabled, children: quantity }) => {
-    switch (type) {
-        case 'recent':
-            return (
-                <div className={cx('quantity', '--recent')}>
-                    <span>🞩</span>
-                    <span>{quantity}</span>
-                </div>
-            );
-        case 'edit':
-            return (
-                <div className={cx('quantity')}>
-                    <button onClick={() => onUpdate(1)} disabled={disabled}>
-                        <FiPlus />
-                    </button>
-                    <span>{quantity}</span>
-                    <button onClick={() => onUpdate(-1)} disabled={disabled}>
-                        {quantity > 1 ? (
-                            <FiMinus />
-                        ) : (
-                            <BiTrashAlt style={{ height: '1.1em', marginBottom: '1px' }} />
-                        )}
-                    </button>
-                </div>
-            );
-        case 'add':
-            return (
-                <button className={cx('quantity')} onClick={onAdd} disabled={disabled}>
-                    <FiPlus />
-                    <span>Add</span>
-                </button>
-            );
-        default:
-            throw new Error('Invalid type prop in QuantitySelect');
-    }
-};
-
-const ConditionalLink = ({ to, condition, children }) =>
-    condition ? <Link to={to}>{children}</Link> : children;
-
-const Dish = ({ dish, recent, standalone }) => {
-    const [cart, dispatchCart] = useCartContext();
+const QuantitySelect = ({ type, dishId, children: quantity }) => {
+    const dispatchCart = useCartContext()[1];
     const [loading, setLoading] = useState(false);
-
-    if (cart === undefined) return null;
-
-    /*  Try to set quantity if dish object contains quantity, 
-        else try to find the dish quantity in the cart,
-        otherwise quantity will be undefined. */
-    const quantity = dish.quantity || cart.find(cartDish => cartDish._id === dish._id)?.quantity;
-
-    let quantitySelectType;
-    if (recent) quantitySelectType = 'recent';
-    else if (quantity) quantitySelectType = 'edit';
-    else quantitySelectType = 'add';
 
     const onUpdate = async change => {
         setLoading(true);
-        const dishId = dish._id;
         const newQuantity = quantity + change;
 
         try {
@@ -84,10 +31,9 @@ const Dish = ({ dish, recent, standalone }) => {
 
     const onAdd = async () => {
         setLoading(true);
-        const _id = dish._id;
 
         try {
-            const res = await dataAPI.post('/user/cart', { _id });
+            const res = await dataAPI.post('/user/cart', { _id: dishId });
             dispatchCart({ type: 'add', payload: { dish: res.data } });
         } catch (err) {
             console.error(err);
@@ -95,6 +41,60 @@ const Dish = ({ dish, recent, standalone }) => {
             setLoading(false);
         }
     };
+
+    switch (type) {
+        case 'recent':
+            return (
+                <div className={cx('quantity', '--recent')}>
+                    <span>🞩</span>
+                    <span>{quantity}</span>
+                </div>
+            );
+        case 'edit':
+            return (
+                <div className={cx('quantity')}>
+                    <button onClick={() => onUpdate(1)} disabled={loading}>
+                        <FiPlus />
+                    </button>
+                    <span>{quantity}</span>
+                    <button onClick={() => onUpdate(-1)} disabled={loading}>
+                        {quantity > 1 ? (
+                            <FiMinus />
+                        ) : (
+                            <BiTrashAlt style={{ height: '1.1em', marginBottom: '1px' }} />
+                        )}
+                    </button>
+                </div>
+            );
+        case 'add':
+            return (
+                <button className={cx('quantity')} onClick={onAdd} disabled={loading}>
+                    <FiPlus />
+                    <span>Add</span>
+                </button>
+            );
+        default:
+            throw new Error('Invalid type prop in QuantitySelect');
+    }
+};
+
+const ConditionalLink = ({ to, condition, children }) =>
+    condition ? <Link to={to}>{children}</Link> : children;
+
+const Dish = ({ dish, recent, standalone }) => {
+    const cart = useCartContext()[0];
+
+    if (cart === undefined) return null;
+
+    /*  Try to set quantity if dish object contains quantity, 
+        else try to find the dish quantity in the cart,
+        otherwise quantity will be undefined. */
+    const quantity = dish.quantity || cart.find(cartDish => cartDish._id === dish._id)?.quantity;
+
+    let quantitySelectType;
+    if (recent) quantitySelectType = 'recent';
+    else if (quantity) quantitySelectType = 'edit';
+    else quantitySelectType = 'add';
 
     return (
         <Card img={{ src: dish.imgUrl, alt: 'dish img' }}>
@@ -114,12 +114,7 @@ const Dish = ({ dish, recent, standalone }) => {
 
             <div className={cx('bottom')}>
                 <span className={cx('price')}>₹ {dish.price}</span>
-                <QuantitySelect
-                    disabled={loading}
-                    type={quantitySelectType}
-                    onUpdate={onUpdate}
-                    onAdd={onAdd}
-                >
+                <QuantitySelect dishId={dish._id} type={quantitySelectType}>
                     {quantity}
                 </QuantitySelect>
             </div>
