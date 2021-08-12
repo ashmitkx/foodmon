604feb4a9a059c75baf811ce4363d892a265d38d
ashmitkx <ashmitk0507@gmail.com>
2021-08-13 01:14:07 +0530

    axios baseUrl fix

--- client/src/api.js
+++ client/src/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 export const dataAPI = axios.create({
-    baseURL: 'http://localhost:5000/api/v1',
+    baseURL: '/api/v1',
     withCredentials: true
 });
 
 export const authAPI = axios.create({
-    baseURL: 'http://localhost:5000/auth',
+    baseURL: '/auth',
     withCredentials: true
 });
