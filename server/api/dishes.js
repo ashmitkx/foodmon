8f19c6f0b683dd3e7e20576137c9c9834d69d921
ashmitkx <ashmitk0507@gmail.com>
2021-08-15 14:50:01 +0530

    api cleanup, build update

--- server/api/dishes.js
+++ server/api/dishes.js
@@ -14,8 +14,7 @@ const getDishes = async (req, res, next) => {
         query = {},
         groupby;
 
-    if (req.query.cuisine) query = { cuisines: { $elemMatch: { $eq: req.query.cuisine } } };
-    else if (req.query.restaurantId) {
+    if (req.query.restaurantId) {
         const restaurantId = req.query.restaurantId;
         // return 400 if restaurantId is an invalid ObjectId
         if (!isValidObjectId(restaurantId))
@@ -81,24 +80,6 @@ const getDishes = async (req, res, next) => {
     }
 
     res.json(doc);
-};
-
-/* 
-    Retuns a list of dishes corresponding to a list of ObjectIds.
-*/
-const getDishesByIds = async (req, res, next) => {
-    const ids = req.params.ids.split(',');
-
-    // return 400 if not all ids are invalid ObjectIds
-    if (!ids.every(id => isValidObjectId(id)))
-        return next({ status: 400, message: 'Invalid dishId(s)' });
-
-    Dishes.find({ _id: { $in: ids } })
-        .then(doc => {
-            if (doc.length === 0) return next({ status: 404 });
-            res.json(doc);
-        })
-        .catch(next);
 };
 
 const dishesApi = { getDishes };
